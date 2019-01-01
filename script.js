4e550215f04e75ee46f9296352f83b12f51b1385
Artur <elkin.as@mail.ru>
2019-01-01 18:22:23 +0300

    matrix smoothing

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let smoothMatrix = [
 	[0, 0, 0]
 ];
 
+// combinations which will add to element's index of matrix for check neighbors elements
 let combinations = [
 	{r: -1, c: -1},
 	{r: -1, c: 0 },
@@ -26,7 +27,14 @@ let combinations = [
 ];
 
 let sum = 0;
+
+// amount of summed elements 
 let elemLength = 0;
+
+// we add item of every combinations to element's index and check is the item 'undefined'
+// if it is false than we add element to sum, and to increment amount of summed elements
+// after we calc average and insert it into smooth matrix 
+// after we reset the variable
 
 for (let row = 0; row < matrix.length; ++row) {
 	for (let col = 0; col < matrix[row].length; ++col) {
@@ -37,11 +45,15 @@ for (let row = 0; row < matrix.length; ++row) {
 				++elemLength;
 			}
 		}
-		smoothMatrix[row][col] = Math.trunc(sum / elemLength * 100) / 100;
+		smoothMatrix[row][col] = Math.trunc(sum / elemLength * 10) / 10;
 		sum = 0;
 		elemLength = 0;
 	}
 }
 
-console.log(smoothMatrix);
+// print smooth matrix
+for (let row of smoothMatrix) {
+	console.log(...row);
+}
+
 
